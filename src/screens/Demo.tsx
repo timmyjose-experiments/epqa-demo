772abbb9359e6b62787280aaa43be9c4eb579043
Timmy Jose <zoltan.jose@gmail.com>
2025-02-18 13:54:06 +0530

    feat: basic project

---
+++ src/screens/Demo.tsx
@@ -0,0 +1,21 @@
+import {useNavigation} from '@react-navigation/native'
+import {NativeStackNavigationProp} from '@react-navigation/native-stack'
+import {RootStackParamList} from '../App'
+import {Pressable, SafeAreaView, Text} from 'react-native'
+import {styles} from '../styles'
+
+const Demo = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate('Home')}>
+        <Text>Home</Text>
+      </Pressable>
+    </SafeAreaView>
+  )
+}
+
+export default Demo
