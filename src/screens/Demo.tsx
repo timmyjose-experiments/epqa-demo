dd1e869c8954b6f3514c8a1500b48c26d037a7aa
Timmy Jose <zoltan.jose@gmail.com>
2025-03-13 18:18:48 +0530

    chore: World -> Mundo

--- src/screens/Demo.tsx
+++ src/screens/Demo.tsx
@@ -10,7 +10,7 @@ const Demo = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello</Text>
-      <Text>World</Text>
+      <Text>Mundo</Text>
       <Pressable
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
